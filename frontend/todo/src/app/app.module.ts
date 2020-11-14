2dd4c98f3776a64182a9e7c393c0769a20bf7c03
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-11-13 23:50:25 -0300

    fixed some git untracked files errors

---
+++ frontend/todo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/todo/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = 'user1';
+  password = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  handleLogin() {
+    console.log(this.username);
+    console.log(this.password);
+
+  }
+
+}

62c64f64cd37ccffcd823dbf8fa0755c209d28a7
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-11-15 18:40:11 -0300

    creating RESTful api in the backend

---
+++ backend/technical-test-backend/src/main/java/com/darkonnen/test/TechnicalTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.darkonnen.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestBackendApplication.class, args);
+	}
+
+}
---
+++ backend/technical-test-backend/src/test/java/com/darkonnen/test/TechnicalTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkonnen.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

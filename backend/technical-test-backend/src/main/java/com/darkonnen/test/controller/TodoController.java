f0b4ecdf9dd54d7142b3120bd2a6e576bef39288
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-11-16 12:27:32 -0300

    finished almost everything, pending some requirements of the technical test

--- backend/technical-test-backend/src/main/java/com/darkonnen/test/controller/TodoController.java
+++ backend/technical-test-backend/src/main/java/com/darkonnen/test/controller/TodoController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +19,7 @@ import com.darkonnen.test.model.Todo;
 import com.darkonnen.test.service.TodoService;
 
 @RestController
-@CrossOrigin(origins = "localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/users")
 public class TodoController {
 
@@ -24,14 +27,15 @@ public class TodoController {
 	private TodoService todoService;
 
 	@GetMapping("/{username}/todos")
-	public List<Todo> getAll(@PathVariable("username") String username) {
-		return todoService.getAll();
+	public ResponseEntity<List<Todo>> getAll(@PathVariable("username") String username) {
+		List<Todo> todos = todoService.getAll();
+		return new ResponseEntity<List<Todo>>(todos, HttpStatus.OK);
 	}
 
 	@PostMapping("/{username}/todos")
-	public Todo create(@PathVariable("username") String username, @Valid Todo todo) {
+	public ResponseEntity<Todo> create(@PathVariable("username") String username, @Valid @RequestBody Todo todo) {
 		Todo t = todoService.create(todo);
-		return t;
+		return new ResponseEntity<Todo>(t, HttpStatus.OK);
 	}
 
 }

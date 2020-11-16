ba3e00af2f3153713bff76d67942b40869c5cb45
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-11-16 15:32:11 -0300

    fixed update method and addedstatic list of 3 users to choose from frontend select option

--- backend/technical-test-backend/src/main/java/com/darkonnen/test/controller/TodoController.java
+++ backend/technical-test-backend/src/main/java/com/darkonnen/test/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.darkonnen.test.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.darkonnen.test.exception.EntityNotFoundException;
 import com.darkonnen.test.model.Todo;
@@ -34,15 +36,29 @@ public class TodoController {
 		List<Todo> todos = todoService.getAll();
 		return new ResponseEntity<List<Todo>>(todos, HttpStatus.OK);
 	}
+	
+	@GetMapping("/{username}/todos/{id}")
+	public ResponseEntity<Todo> getTodo(@PathVariable("username") String username, @PathVariable("id") Integer id) {
+		Todo todo = todoService.getById(id);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+	}
+
 
 	@PostMapping("/{username}/todos")
 	public ResponseEntity<Todo> create(@PathVariable("username") String username, @Valid @RequestBody Todo todo) {
 		Todo t = todoService.create(todo);
-		return new ResponseEntity<Todo>(t, HttpStatus.OK);
+//		return new ResponseEntity<Todo>(t, HttpStatus.OK);
+		
+//		----------------
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(t.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
 	}
 	
-	@PutMapping("/{username}/todos")
-	public ResponseEntity<Todo> update(@PathVariable("username") String username, @Valid @RequestBody Todo todo) {
+	@PutMapping("/{username}/todos/{id}")
+	public ResponseEntity<Todo> update(@PathVariable("username") String username, @PathVariable("id") Integer id, @Valid @RequestBody Todo todo) {
 		Todo t = todoService.update(todo);
 		return new ResponseEntity<Todo>(t, HttpStatus.OK);
 	}
